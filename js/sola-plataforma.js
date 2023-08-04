4a320510ed8d8444a2a616fee6125394ad32a855
emanuel <emanueldavidroca@gmail.com>
2023-08-04 14:04:51 -0300

    textos y animaciones

--- js/sola-plataforma.js
+++ js/sola-plataforma.js
@@ -6,26 +6,32 @@ hexagono.forEach(hex => {
         if(hex.classList.contains("administrar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Administrar informacion <br> Administrar en tiempo real.";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "18.5%";
         }
         if(hex.classList.contains("gestionar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Gestionar base de datos <br> Control de variables.";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "18.5%";
         }
         if(hex.classList.contains("integrar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Integracion facil <br> Soporte tecnico.";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "18.5%";
         }
         if(hex.classList.contains("diseñar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Diseños personalizado <br> Configurable y maquetable.";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "18.5%";
         }
         if(hex.classList.contains("operar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "24/7 tiempo operacional <br> Disponibilidad continua.";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "18.5%";
         }
         if(hex.classList.contains("conectar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Conectar dispositivos <br> Conexion multiplataforma.";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "18.5%";
         }
     });
 });
