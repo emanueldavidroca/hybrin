399fbf633b8ef9c54fbe05572d4239c67863c012
emanuel <emanueldavidroca@gmail.com>
2023-08-04 14:06:27 -0300

    textos y animaciones

--- js/sola-plataforma.js
+++ js/sola-plataforma.js
@@ -7,31 +7,37 @@ hexagono.forEach(hex => {
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Administrar informacion <br> Administrar en tiempo real.";
             document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "18.5%";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "unset";
         }
         if(hex.classList.contains("gestionar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Gestionar base de datos <br> Control de variables.";
             document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "18.5%";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "unset";
         }
         if(hex.classList.contains("integrar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Integracion facil <br> Soporte tecnico.";
             document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "18.5%";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "unset";
         }
         if(hex.classList.contains("diseñar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Diseños personalizado <br> Configurable y maquetable.";
             document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "18.5%";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "unset";
         }
         if(hex.classList.contains("operar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "24/7 tiempo operacional <br> Disponibilidad continua.";
             document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "18.5%";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "unset";
         }
         if(hex.classList.contains("conectar")){
             document.querySelector("#section-sola-plataforma-desktop .texto").innerHTML = 
             "Conectar dispositivos <br> Conexion multiplataforma.";
             document.querySelector("#section-sola-plataforma-desktop .texto").style.right= "18.5%";
+            document.querySelector("#section-sola-plataforma-desktop .texto").style.left= "unset";
         }
     });
 });
